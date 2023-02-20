ad9544100b65dcf25e58b923e2b4adebdac46595
kypkk <kypss50102@gmail.com>
2023-02-20 10:49:43 +0800

    chore: update update_brewfile.sh

--- update_brewfile.sh
+++ update_brewfile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -x
 # rm Brewfile Brewfile.lock.json
-cd ~
 brew bundle dump --describe --force
-chezmoi cd
-chezmoi add --template ~/Brewfile
+chezmoi add --template /Brewfile
 git add Brewfile.tmpl
 git commit -m "chore: update Brewfile.tmpl"
 
